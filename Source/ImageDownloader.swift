737d65d19c221fd008fab4c3bb350ea1b20dbdd8
Ivan Misuno <im@popappfactory.com>
2016-03-03 17:59:46 +0100

    Rename Error -> AlamofireError to fix naming conflict with Realm

--- Source/ImageDownloader.swift
+++ Source/ImageDownloader.swift
@@ -421,7 +421,7 @@ public class ImageDownloader {
                     let error: NSError = {
                         let failureReason = "ImageDownloader cancelled URL request: \(URLRequest.URLString)"
                         let userInfo = [NSLocalizedFailureReasonErrorKey: failureReason]
-                        return NSError(domain: Error.Domain, code: NSURLErrorCancelled, userInfo: userInfo)
+                        return NSError(domain: AlamofireError.Domain, code: NSURLErrorCancelled, userInfo: userInfo)
                     }()
 
                     return Response(request: URLRequest, response: nil, data: nil, result: .Failure(error))
