bdfc7301293f2e60d270645bb869798b56409381
Ivan Misuno <im@popappfactory.com>
2016-07-24 20:24:24 +0200

    Comment out #imports -> include as source.

--- Source/ImageDownloader.swift
+++ Source/ImageDownloader.swift
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import Alamofire
+//import Alamofire
 import Foundation
 
 #if os(iOS) || os(tvOS) || os(watchOS)
